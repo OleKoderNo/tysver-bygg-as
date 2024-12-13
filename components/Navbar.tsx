20d7dae52e47109f07bfa8741001e25064b58272
Ole <oleh.kodehode@gmail.com>
2024-12-13 05:58:37 +0100

    nav bar is on spot

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,19 @@
-// import { useState } from "react";
-// import Image from "next/image";
+import { useState } from "react";
+import Image from "next/image";
+import { GiHamburgerMenu } from "react-icons/gi";
 
-// const Navbar = () => {
-//   const [menuOpen, setMenuOpen] = useState(false);
+const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-//   return (
-//     <nav className="absolute">
-//       <p>Tysær Bygg AS</p>
-//     </nav>
-//   );
-// };
+  return (
+    <nav className="text-white px-4">
+      <p className="text-lg font-bold">Tysær Bygg AS</p>
+      <GiHamburgerMenu
+        className="text-2xl cursor-pointer"
+        onClick={() => setMenuOpen(!menuOpen)}
+      />
+    </nav>
+  );
+};
 
-// export default Navbar;
+export default Navbar;
